08cd2360e77d83af08fa26a6f9faf2c8cdc07d51
FLXwkg <ewenheas13@gmail.com>
2023-12-28 02:34:12 +0100

    :card_file_box: : Changement de nom de DB

--- api/db.js
+++ api/db.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
   password: '',
-  database: 'badminton',
+  database: 'reservation_badminton',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
